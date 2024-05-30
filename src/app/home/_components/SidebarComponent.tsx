09525f594afbf4544130dfed0499cd36f8d47091
Amarthya K R <amarthya10@gmail.com>
2024-05-30 18:28:49 +0530

    [REFACTOR]: Routes modified to - reidentification - trafficcounts

--- src/app/home/_components/SidebarComponent.tsx
+++ src/app/home/_components/SidebarComponent.tsx
@@ -220,6 +220,7 @@ export default function MiniDrawer() {
 							key={index}
 							href={`/home/${text
 								.replace(" ", "")
+								.replace("-", "")
 								.toLowerCase()}`}
 						>
 							<ListItem
