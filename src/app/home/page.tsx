6e8be89b2bd8f4669878cb4583aba87ab6509660
Ubuntu <ubuntu@ip-172-31-45-3.ap-south-1.compute.internal>
2024-06-06 11:12:20 +0000

    some local changes

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Image from 'next/image';
 
 const page = () => {
-	return <div>This is a home page</div>;
+	return <div>
+      <h3> Bangalore Road Traffic Analytics Dashboard </h3>
+      <img
+        src="https://ichef.bbci.co.uk/news/976/cpsprodpb/8CFC/production/_92729063_newairportroadnearhebbal.jpg" // Path to your image relative to the public directory
+        alt="Traffic"
+      />
+    </div>;
 };
 
 export default page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
-	return <div>This is the root page</div>;
-}
+import React from "react";
+import Image from 'next/image';
+
+const page = () => {
+	return <div>
+      <h3> Bangalore Road Traffic Analytics Dashboard </h3>
+      <img
+        src="https://ichef.bbci.co.uk/news/976/cpsprodpb/8CFC/production/_92729063_newairportroadnearhebbal.jpg" // Path to your image relative to the public directory
+        alt="Traffic"
+      />
+    </div>;
+};
+
+export default page;
