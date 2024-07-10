32e61aae19eb8b63466bd1d8d8ca88e74a1d29fc
Amarthya K R <amarthya10@gmail.com>
2024-07-10 22:36:18 +0530

    [REFACTOR]: New iframe added

--- src/app/home/observe/violations/page.tsx
+++ src/app/home/observe/violations/page.tsx
@@ -1,3 +1,12 @@
 export default function Page() {
-	return <div>Violations</div>;
+	return (
+		<div>
+			<iframe
+				width="100%"
+				height="900px"
+				src="http://3.110.136.33:8000/abstreet.html"
+				// title="W3Schools Free Online Web Tutorials"
+			></iframe>
+		</div>
+	);
 }
